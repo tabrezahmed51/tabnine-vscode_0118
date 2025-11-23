961e4baf1b1c168c5842429e1436d09ab12ba5eb
Tabrez Ahmed <225601255+tabrezahmed51@users.noreply.github.com>
2025-11-23 21:37:11 +0530

    Remove all enterprise/paid features - keep only free/open-source functionality

--- src/enterprise/update/confirmReload.ts
+++
@@ -1,9 +0,0 @@
-import { commands } from "vscode";
-import confirm from "./confirm";
-import { RELOAD_BUTTON_LABEL, RELOAD_COMMAND } from "../consts";
-
-export default async function confirmReload(message: string): Promise<void> {
-  if (await confirm(message, RELOAD_BUTTON_LABEL)) {
-    await commands.executeCommand(RELOAD_COMMAND);
-  }
-}
